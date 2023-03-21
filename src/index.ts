d9cae0e2d87d7d01b56723b620e8ab7cfaa27a7b
Dec Murphy <okdecm@gmail.com>
2023-03-21 23:56:38 +0000

    Exposed `ValidationError`

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { Fn } from "hotscript";
 export { HTTPSchema, HTTPResponseSchema } from "./HTTP/Schema";
 export { httpClient } from "./HTTP/Client";
 export { httpRouter, HandlerCallback } from "./HTTP/Router";
+
+export { ValidationError } from "HTTP/ValidationError";
